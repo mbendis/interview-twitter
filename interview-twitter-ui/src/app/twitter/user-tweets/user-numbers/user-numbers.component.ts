98d738bd8d0e94dafe8302d1fbbaa16dee5a0486
Marin Bendis <marin.bendis@gmail.com>
2019-03-13 16:43:09 +0100

    Added tweets count.

--- interview-twitter-ui/src/app/twitter/user-tweets/user-numbers/user-numbers.component.ts
+++ interview-twitter-ui/src/app/twitter/user-tweets/user-numbers/user-numbers.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { UserService } from '../../../services/user/user.service';
 import {HttpClient} from '@angular/common/http';
+import {TweetService} from "../../../services/tweet/tweet.service";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-user-numbers',
@@ -12,7 +14,7 @@ export class UserNumbersComponent implements OnInit {
     followers: number;
     following: number;
     tweetsCount: number;
-    constructor(private userService: UserService){
+    constructor(private userService: UserService, private tweetService: TweetService, private authService: AuthService){
     }
     ngOnInit(): void {
         console.log('On init');
@@ -20,6 +22,8 @@ export class UserNumbersComponent implements OnInit {
           followers => this.followers = followers.length);
         this.userService.fetchFollowing().subscribe(
                 following => this.following = following.length);
+        this.tweetService.fetchForUser(this.authService.getCurrentUser()).subscribe(
+                      tweets => this.tweetsCount = tweets.length);
 
     }
 }
