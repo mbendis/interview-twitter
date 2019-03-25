5ddddbf07f29e47999b64d45009f1c679dc2a66a
FER\mb49390 <marin.bendis@fer.com>
2019-03-25 15:09:50 +0100

    Added registration container component.

---
+++ interview-twitter-ui/src/app/auth/registration-container/registration-container.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'registration-container.component.html'
+})
+export class RegistrationContainerComponent {
+}
